0b76855eca622f90498bafb5e6da83835f5ffbd8
Elegantniy <maxim9083@gmail.com>
2023-06-20 19:37:25 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # node-hw1
+
+
+
+list https://ibb.co/25cb5mp
+get https://ibb.co/N2xMRqd
+add https://ibb.co/5KT09T2
+remove https://ibb.co/G3fWgck
--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Hello world")
+const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const justOneContact = await contacts.getContactById(id);
+      return console.log(justOneContact);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      return console.log(removeContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
